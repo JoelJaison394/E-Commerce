2775869e78a2d80e42c69c3ec757c30d2bfa3839
Joel Jaison <joeljaison394@gmail.com>
2023-02-04 22:59:09 +0530

    product update route added

--- backend/controllers/productContoller.js
+++ backend/controllers/productContoller.js
@@ -1,20 +1,47 @@
-const Product = require("../Models/productModel")
-
-
+const { addListener } = require("../app");
+const Product = require("../Models/productModel");
 
 // Create Product
-exports.createProduct = async (req,res,next)=>{
-    const product = await Product.create(req.body);
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-    res.status(201)
-    .json({
-        success:true,
-        product
-    })
-}
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
 
+// Get All Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200)
-    .json({message:"Route is working"})    
-}
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//Update Product -- Admin route
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: failed,
+      message: "product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200)
+  .json({
+    success:true,
+    product
+  })
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getAllProducts,createProduct } = require("../controllers/productContoller");
+const { getAllProducts,createProduct, updateProduct } = require("../controllers/productContoller");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router
